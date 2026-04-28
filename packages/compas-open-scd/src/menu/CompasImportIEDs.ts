d926b56a79c1481b11bc45b445b5e7c3bc11024d
Christopher Lepski <c.lepski@sprinteins.com>
2026-04-28 11:58:56 +0200

    chore: Update packages

--- packages/compas-open-scd/src/menu/CompasImportIEDs.ts
+++ packages/compas-open-scd/src/menu/CompasImportIEDs.ts
@@ -1,4 +1,4 @@
-import { html, query, TemplateResult } from 'lit-element';
+import { query, TemplateResult } from 'lit-element';
 import { translate } from 'lit-translate';
 
 import ImportingIedPlugin from '@compas-oscd/plugins/dist/menu/ImportIEDs.js';
@@ -15,7 +15,7 @@ export default class CompasImportIEDSMenuPlugin extends ImportingIedPlugin {
   compasOpen!: Dialog;
 
   renderInput(): TemplateResult {
-    return html`<mwc-dialog
+    return this.componentHtml`<mwc-dialog
       id="compas-import-ieds-dlg"
       heading="${translate('compas.open.title')}"
     >
