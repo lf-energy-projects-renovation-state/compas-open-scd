8ae5ccabd8539082892dda0ca17a2a9f20ba5a9a
Christopher Lepski <c.lepski@sprinteins.com>
2026-05-20 16:39:28 +0200

    chore: Server external plugins as static content

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,13 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        { src: 'packages/external-plugins/**/*', dest: 'external-plugins', rename: { stripBase: 2 } },
+        { src: 'node_modules/@compas-oscd/plugins/**/*', dest: 'plugins', rename: { stripBase: 3 } }
+      ]
+    })
+  ]
 })
