9a1a51fa123536ad00565372a58b4222c591c19a
Stiliyan Valkanov <stiliyan.valkanov@bearingpoint.com>
2026-08-26 10:17:31 +0200

    fix: restore old path to IedEditor plugin
    Signed-off-by: Stiliyan Valkanov <stiliyan.valkanov@bearingpoint.com>

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -1,7 +1,7 @@
 export const officialPlugins = [
   {
     name: 'IED',
-    src: '/external-plugins/oscd-editor-ied/oscd-editor-ied.js',
+    src: '/external-plugins/IedEditor.js',
     icon: 'developer_board',
     activeByDefault: true,
     kind: 'editor',
