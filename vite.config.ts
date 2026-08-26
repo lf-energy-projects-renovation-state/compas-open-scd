9ce2793c6a4a345a8305cfacbd80047583fdfa0f
Stiliyan Valkanov <stiliyan.valkanov@bearingpoint.com>
2026-08-26 11:41:18 +0200

    fix: check local external-plugins directory availability before static copy
    Signed-off-by: Stiliyan Valkanov <stiliyan.valkanov@bearingpoint.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { existsSync } from 'node:fs'
+
+const externalPluginsPath = (existsSync('distribution/external-plugins')
+  ? [{ src: 'distribution/external-plugins/**/*', dest: 'external-plugins', rename: { stripBase: 2 } }]
+  : [])
 
 export default defineConfig({
   plugins: [
     viteStaticCopy({
       targets: [
-        { src: 'distribution/external-plugins/**/*', dest: 'external-plugins', rename: { stripBase: 2 } },
+        ...externalPluginsPath,
         { src: 'packages/external-plugins/**/*', dest: 'external-plugins', rename: { stripBase: 2 } },
         { src: 'packages/external-plugins/IedEditor.js', dest: 'external-plugins', rename: { stripBase: 2 } }
       ]
